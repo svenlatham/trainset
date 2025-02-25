a7fe5d068d000c2605ae1074ee964d3b6edfacb0
Sven Latham <sven@svenlatham.com>
2025-02-25 08:04:41 +0000

    Update main.py
    swapped ch1/2 and ch4/5

--- src/main.py
+++ src/main.py
@@ -28,11 +28,11 @@ from signal import *
 
 # Setup:
 
-ch1 = RedGreenSignal('CH1', 16, 17)
-ch2 = RedGreenSignal('CH2', 18, 19)
+ch1 = RedGreenSignal('CH1', 18, 19)
+ch2 = RedGreenSignal('CH2', 16, 17)
 ch3 = RedGreenSignal('CH3', 20, 21)
-ch4 = RedGreenSignal('CH4', 15, 14)
-ch5 = RedGreenSignal('CH5', 13, 12)
+ch4 = RedGreenSignal('CH4', 13, 12)
+ch5 = RedGreenSignal('CH5', 15, 14)
 
 RedGreenSignal.setAllRed([ch1, ch2, ch3, ch4, ch5])
 # Set a path from CH5 to CH1
